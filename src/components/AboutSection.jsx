dc1a87f88a5d4566c851e77597d4f28ca7c753b9
kumarshivam1998 <lookup.shivam@gmail.com>
2025-10-05 17:06:13 +0530

    added drive link

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="https://drive.google.com/file/d/1AwyaZxTkvRY1AyKmgi2l3bx_ORF28b12/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
